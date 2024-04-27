37a050f1303ccf71aabb33bdddb7672a2141100a
Juliusz <juliusz.czechura@gmail.com>
2024-04-27 17:35:23 +0200

    Added web host handling

--- CarRental/Program.cs
+++ CarRental/Program.cs
@@ -1,1 +1,17 @@
-Console.WriteLine("Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+app.UseHttpsRedirection();
+
+app.Run();
